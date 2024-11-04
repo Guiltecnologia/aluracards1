7467e777bb05ad1d0fcc085412230134b98aa144
Guiltecnologia <00001117697824sp@al.educacao.sp.gov.br>
2024-11-04 11:06:31 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Esportes',
+    'qual o nomes do jogador que ganhou 3 copas do mundo ?',
+    'a resposta é Pelé'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da argentina?',
+    'A capital da argentina é Bueno ares'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz obrigado em ingles ?',
+    'Em ingles se diz thank'
+)
+
+criaCartao(
+    'Esportes',
+    'Qual o nomes do jogador de basquete que participou do filme space jam de 1996?',
+    'O nome do jogador é Michael Jordan'
+)
